157e575c79f5d81a2c43b9a8c509ec27057455e1
Apathy <apathyhill@gmail.com>
2019-10-29 15:20:43 -0400

    a
    a

--- setup.py
+++ setup.py
@@ -3,20 +3,25 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+REQUIRED = [
+    "numpy",
+    "pandas"
+]
+
 setuptools.setup(
-    name="lambdata-apathyhill", # Replace with your own username
-    version="0.0.1",
+    name="lambdata-apathyhill",
+    version="0.0.5",
     author="ApathyHill",
-    author_email="apathyhill@gmail.com",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/apathyhill/Lambdata",
     packages=setuptools.find_packages(),
+    install_requires = REQUIRED,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.5',
 )
