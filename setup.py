40c875883cc44a8fc6302a179ee7d558a39cdb93
Apathy <apathyhill@gmail.com>
2019-11-01 15:13:20 -0400

    a

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ REQUIRED = [
 
 setuptools.setup(
     name="lambdata-apathyhill",
-    version="0.0.13",
+    version="0.1.0",
     author="ApathyHill",
     description="A small example package",
     long_description=long_description,
